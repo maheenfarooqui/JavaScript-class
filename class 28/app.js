03e41a62d42edb5ad3d422ac33bc6f8e84192d79
maheen <maheenzuhra@gmail.com>
2026-03-05 22:44:07 +0500

    update

---
+++ class 28/app.js
@@ -0,0 +1,14 @@
+var para = document.getElementsByTagName("p");
+function colorChange(){
+for(var i = 0 ; i < para.length ; i++){
+      para[i].style.color = "yellow" 
+}
+
+}
+function noColor(){
+for(var i = 0 ; i < para.length ; i++){
+    
+      para[i].style.color = "red" 
+}
+
+}
